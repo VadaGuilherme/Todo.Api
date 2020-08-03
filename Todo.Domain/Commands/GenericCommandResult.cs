0de8a54b98b912a1105330ea2b7af6c1a487b48c
Guilherme Vada <mail@guilhermevada.com.br>
2020-08-02 21:35:27 -0300

    Created Handler CreateTodoCommand

--- Todo.Domain/Commands/GenericCommandResult.cs
+++ Todo.Domain/Commands/GenericCommandResult.cs
@@ -4,6 +4,17 @@ namespace Todo.Domain.Commands
 {
   public class GenericCommandResult : ICommandResult
   {
+    public GenericCommandResult()
+    {
+
+    }
+    public GenericCommandResult(bool success, string message, object data)
+    {
+      this.Success = success;
+      this.Message = message;
+      this.Data = data;
+
+    }
     public bool Success { get; set; }
     public string Message { get; set; }
     public object Data { get; set; }
